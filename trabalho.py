578f9c7e28d59ea07630801dd3fd2a6d5036e0ad
Lucas Neves <lucas54neves@gmail.com>
2019-10-27 14:06:15 -0300

    Leitura dos dados finalizada

--- trabalho.py
+++ trabalho.py
@@ -1,29 +1,39 @@
 #!/usr/bin/python
-# -*- coding: latin-1 -*-
+# -*- coding: utf-8 -*-
 
 import xlrd
 import datetime
 import math
 import time
 
-# Classe que representa as restricoes de horarios do professores
-class Restricao:
-    def __init__(self, indice_professor, horario, dia):
-        self.indice_professor = indice_professor
-        self.horario = horario
+# Classe que representa o horario
+class Horario:
+    def __init__(self, hora, dia):
+        self.hora = hora
         self.dia = dia
 
-    def get_indice_professor(self):
-        return self.indice_professor
-
-    def get_horario(self):
-        return self.horario
+    def get_hora(self):
+        return self.hora
 
     def get_dia(self):
         return self.dia
 
+# Classe que representa as restricoes de horarios dos professores ou das turma
+class Restricao:
+    def __init__(self, indice, horario):
+        # Indice do professor ou da turma
+        self.indice = indice
+        # Horario de restricao
+        self.horario = horario
+
+    def get_indice(self):
+        return self.indice
+
+    def get_horario(self):
+        return self.horario
+
     def imprimir(self):
-        print(self.indice_professor, self.horario, self.dia)
+        print(self.indice, self.horario.get_hora(), self.horario.get_dia())
 
 # Classe que representa as disciplinas
 class Disciplina:
@@ -70,7 +80,7 @@ class Cor:
         return self.horario
 
     def imprimir(self):
-        print(self.indice, self.horario)
+        print(self.indice, self.horario.get_hora(), self.horario.get_dia())
 
 # Classe que representa os vertices
 class Vertice:
@@ -102,11 +112,15 @@ class Grafo:
         # Lista de adjacencia entre os vertices
         self.lista_adjacencia = []
         # Lista que guarda os horarios
-        self.lista_horarios = []
+        self.lista_horas = []
         # Lista que guarda as restricoes de horarios dos professores
         self.lista_restricoes_professores = []
         # Lista que guarda as cores relativas aos horarios
         self.lista_cores = []
+        # Lista que guarda as restricoes dos horarios das turmas
+        self.lista_restricoes_turmas = []
+        # Lista que guarda as preferencias de cada professor
+        self.lista_preferencias_professores = []
 
     def leitura(self, nome_arquivo):
         # Abre o arquivo
@@ -120,6 +134,12 @@ class Grafo:
 
         # Realiza a leitura da aba Restricoes
         self.leitura_restricoes(planilha)
+
+        # Realiza a leitura da aba Restricoes Turma
+        self.leitura_restricoes_turma(planilha)
+
+        # Realiza a leitura da aba Preferencias
+        self.leitura_preferencias(planilha)
 
     def leitura_dados(self, arquivo):
         # Pega a primeira aba da planilha, a aba Dados
@@ -152,9 +172,6 @@ class Grafo:
         # Criar a lista de adjacencia porque precisa da quantidade de vertices
         self.construir_lista_adjacencia()
 
-        # for vertice in self.lista_vertices:
-        #     vertice.imprimir()
-
     def leitura_configuracoes(self, arquivo):
         # Pega a segunda aba da planilha, a aba Configuracoes
         aba = arquivo.sheet_by_index(1)
@@ -163,19 +180,10 @@ class Grafo:
         for i in xrange(aba.nrows):
             if i != 0:
                 valores = aba.row_values(i)
-                horario = float(valores[0])
-                # x = float(valores[0]) * 24 * 3600
-                #self.adicionar_horario(datetime.time(int(x/3600), int((x%3600)/60), int(x%60)))
-                # self.adicionar_horario(time.strftime('%H:%M:%S',x))
-                #time.strftime('%H:%M:%S', time.gmtime(12345))
-                self.adicionar_horario(horario)
+                hora = float(valores[0])
+                self.adicionar_hora(hora)
 
         self.adicionar_cores()
-
-        # Teste de leitura
-        # print("Configuracoes")
-        # for i in self.lista_horarios:
-        #     print(i)
 
     def leitura_restricoes(self, arquivo):
         # Pega a terceira aba da planilha, a aba Restricoes
@@ -191,25 +199,73 @@ class Grafo:
                 professor = valores[0].split()
                 indice_professor = int(professor[1])
 
-                # Pega o horario
-                horario = valores[1]
+                # Pega a hora
+                hora = valores[1]
 
                 # Pega o dia da semana
                 dia = valores[2].encode('utf-8')
 
-                self.adicionar_restricao(indice_professor, horario, dia)
+                self.adicionar_restricao_professores(indice_professor, Horario(hora, dia))
 
-        # for i in self.lista_restricoes_professores:
-        #     i.imprimir()
+    def leitura_restricoes_turma(self, arquivo):
+        # Pega a quarta aba da planilha, a aba Restricoes Turma
+        # Nessa aba esta os horarios de restricao para cada turma
+        aba = arquivo.sheet_by_index(3)
+
+        # Loop para leitura dos dados
+        for i in xrange(aba.nrows):
+            if i != 0:
+                valores = aba.row_values(i)
+
+                # Pega o numero da turma
+                turma = int(valores[0])
+
+                # Pega a hora
+                hora = float(valores[1])
+
+                # Pega o dia
+                dia = valores[2].encode('utf-8')
+
+                self.adicionar_restricao_turmas(turma, Horario(hora, dia))
+
+    def leitura_preferencias(self, arquivo):
+        # Pega a quinta aba da planilha, a aba Preferencias
+        # Nessa aba esta os horarios de preferencias de cada professor
+        aba = arquivo.sheet_by_index(4)
+
+        # Loop para leitura dos dados
+        for i in xrange(aba.nrows):
+            if i != 0:
+                valores = aba.row_values(i)
+
+                # Pega o indice do professor
+                professor = valores[0].split()
+                indice_professor = int(professor[1])
+
+                # Pega a hora
+                hora = float(valores[1])
+
+                # Pega o dia
+                dia = valores[2].encode('utf-8')
+
+                self.adicionar_preferencias_professores(indice_professor, Horario(hora, dia))
 
     def adicionar_vertice(self, indice, disciplina):
         self.lista_vertices.append(Vertice(indice, disciplina))
 
-    def adicionar_horario(self, horario):
-        self.lista_horarios.append(horario)
+    def adicionar_hora(self, hora):
+        self.lista_horas.append(hora)
 
-    def adicionar_restricao(self, indice_professor, horario, dia):
-        self.lista_restricoes_professores.append(Restricao(indice_professor, horario, dia))
+    def adicionar_restricao_professores(self, indice_professor, horario):
+        self.lista_restricoes_professores.append(Restricao(indice_professor, horario))
+
+    def adicionar_restricao_turmas(self, turma, horario):
+        self.lista_restricoes_turmas.append(Restricao(turma, horario))
+
+    # Para adicionar as preferencias dos professores esta sendo aproveitada a
+    # classe Restricao
+    def adicionar_preferencias_professores(self, professor, horario):
+        self.lista_preferencias_professores.append(Restricao(professor, horario))
 
     def construir_lista_adjacencia(self):
         self.quantidade_vertices = len(self.lista_vertices)
@@ -231,22 +287,27 @@ class Grafo:
         for vertice1 in self.lista_vertices:
             for vertice2 in self.lista_vertices:
                 if vertice1 != vertice2:
+                    # nao eh permitida a alocacao de duas aulas para um
+                    # mesmo professor no mesmo horario
                     if vertice1.get_disciplina().get_professor() == vertice2.get_disciplina().get_professor():
                         self.criar_aresta(vertice1.get_indice(), vertice2.get_indice())
+                    # nao pode haver duas aulas para uma mesma turma no
+                    # mesmo horario
                     if vertice1.get_disciplina().get_turma() == vertice2.get_disciplina().get_turma():
                         self.criar_aresta(vertice1.get_indice(), vertice2.get_indice())
 
     def adicionar_cores(self):
         dias = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta")
 
+        # indice para as cores
         i = 0
         for dia in dias:
-            for horario in self.lista_horarios:
-                self.lista_cores.append(Cor(i, (horario, dia)))
+            for hora in self.lista_horas:
+                self.lista_cores.append(Cor(i, Horario(hora, dia)))
                 i += 1
 
-        for cor in self.lista_cores:
-            cor.imprimir()
+    # Metodo que colore os vertices
+    # def colore(self):
 
 grafo = Grafo()
 grafo.leitura("dados/Escola_A.xlsx")
