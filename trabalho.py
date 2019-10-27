434616da79516acd256915b53d6a380248564e96
Lucas Neves <lucas54neves@gmail.com>
2019-10-27 13:26:15 -0300

    Lista de horários e cores

--- trabalho.py
+++ trabalho.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
 import xlrd
 import datetime
 import math
@@ -54,6 +57,21 @@ class Disciplina:
     def imprimir(self):
         print(self.indice, self.materia, self.turma, self.professor, self.quantidade_aulas)
 
+# Classe que representa as cores
+class Cor:
+    def __init__(self, indice, horario):
+        self.indice = indice
+        self.horario = horario
+
+    def get_indice(self):
+        return self.indice
+
+    def get_horario(self):
+        return self.horario
+
+    def imprimir(self):
+        print(self.indice, self.horario)
+
 # Classe que representa os vertices
 class Vertice:
     def __init__(self, indice, disciplina):
@@ -87,6 +105,8 @@ class Grafo:
         self.lista_horarios = []
         # Lista que guarda as restricoes de horarios dos professores
         self.lista_restricoes_professores = []
+        # Lista que guarda as cores relativas aos horarios
+        self.lista_cores = []
 
     def leitura(self, nome_arquivo):
         # Abre o arquivo
@@ -132,8 +152,8 @@ class Grafo:
         # Criar a lista de adjacencia porque precisa da quantidade de vertices
         self.construir_lista_adjacencia()
 
-        for vertice in self.lista_vertices:
-            vertice.imprimir()
+        # for vertice in self.lista_vertices:
+        #     vertice.imprimir()
 
     def leitura_configuracoes(self, arquivo):
         # Pega a segunda aba da planilha, a aba Configuracoes
@@ -143,11 +163,14 @@ class Grafo:
         for i in xrange(aba.nrows):
             if i != 0:
                 valores = aba.row_values(i)
-                x = float(valores[0]) * 24 * 3600
+                horario = float(valores[0])
+                # x = float(valores[0]) * 24 * 3600
                 #self.adicionar_horario(datetime.time(int(x/3600), int((x%3600)/60), int(x%60)))
                 # self.adicionar_horario(time.strftime('%H:%M:%S',x))
                 #time.strftime('%H:%M:%S', time.gmtime(12345))
+                self.adicionar_horario(horario)
 
+        self.adicionar_cores()
 
         # Teste de leitura
         # print("Configuracoes")
@@ -212,6 +235,18 @@ class Grafo:
                         self.criar_aresta(vertice1.get_indice(), vertice2.get_indice())
                     if vertice1.get_disciplina().get_turma() == vertice2.get_disciplina().get_turma():
                         self.criar_aresta(vertice1.get_indice(), vertice2.get_indice())
+
+    def adicionar_cores(self):
+        dias = ("Segunda", "Terça", "Quarta", "Quinta", "Sexta")
+
+        i = 0
+        for dia in dias:
+            for horario in self.lista_horarios:
+                self.lista_cores.append(Cor(i, (horario, dia)))
+                i += 1
+
+        for cor in self.lista_cores:
+            cor.imprimir()
 
 grafo = Grafo()
 grafo.leitura("dados/Escola_A.xlsx")
